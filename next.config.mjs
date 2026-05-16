bea0aa496193f3a64dd7e607c32dc5d34f3b1b39
TalhaWaseem114 <talha@groworbit.com>
2026-05-17 03:29:58 +0500

    chore: enable static export for Cloudflare and Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -12,3 +14,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+

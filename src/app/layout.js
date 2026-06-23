d513b8b177b26c3e70c05f8b3acd6deab58a4c79
TalhaWaseem114 <talha@groworbit.com>
2026-06-23 19:11:18 +0500

    fix: hardcode Meta Pixel ID with beforeInteractive strategy for reliable detection

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,6 @@ const zenDots = Zen_Dots({
   display: "swap",
 });
 
-
 const playfair = Playfair_Display({
   subsets: ["latin"],
   style: ["normal", "italic"],
@@ -48,25 +47,40 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
-  const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
-  const hasMetaPixel = pixelId && pixelId !== "YOUR_PIXEL_ID";
-
   return (
     <html lang="en" className={`${inter.variable} ${montserrat.variable} ${zenDots.variable} ${playfair.variable}`}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        {hasMetaPixel && (
-          <noscript>
-            <img
-              height="1"
-              width="1"
-              style={{ display: "none" }}
-              src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
-              alt=""
-            />
-          </noscript>
-        )}
+
+        {/* Meta Pixel Code - hardcoded for reliable detection */}
+        <Script
+          id="meta-pixel"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1743928903460669');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1743928903460669&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body className="antialiased font-sans" suppressHydrationWarning>
         <PWARegister />
@@ -74,27 +88,6 @@ export default function RootLayout({ children }) {
         <Analytics />
         <SpeedInsights />
         <ClientSideFeatures />
-
-        {hasMetaPixel && (
-          <Script
-            id="meta-pixel"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '${pixelId}');
-                fbq('track', 'PageView');
-              `,
-            }}
-          />
-        )}
       </body>
     </html>
   );
